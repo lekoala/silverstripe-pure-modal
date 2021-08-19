38b14ec1fe02b157b2c538495129f004789be100
Thomas <thomas@lekoala.be>
2021-08-19 10:32:50 +0200

    simplify resize code

--- client/pure-modal.js
+++ client/pure-modal.js
@@ -1,17 +1,6 @@
 function resizeIframe(iframe) {
-    var $obj = jQuery(iframe);
-    var height = $obj.contents().height();
-    if ($obj.is(":visible") || height == 0) {
-        var clone = $obj.clone().attr("id", false).css({
-            visibility: "hidden",
-            display: "block",
-            position: "absolute",
-        });
-        jQuery("body").append(clone);
-        height = clone.height();
-        clone.remove();
-    }
-    if (height > 0) {
-        $obj.height(height);
-    }
+    iframe.onload = function () {
+        iframe.style.height =
+            iframe.contentWindow.document.body.scrollHeight + "px";
+    };
 }
